c1060ea04ad37a47732a6f52c470ffa4b031b3af
Yan-Fa Li <yanfali@gmail.com>
2013-06-01 00:12:56 -0700

    Add some JavaScript actions. WIP.
    I'm trying to avoid using backbone for the login
    screen and just use plain jQuery with require.
    It's a lot messier.
    
    This code needs a lot of refactoring.
    - add a remove text button when text is present in input box
    - add a submission click handler to anchor
    - prevent submissions faster than every second
    - add some basic code to detect when there's something to remove
      and show and hide the remove button when there's at least 1 character
    - remove bootstrap.js from login page, we're only using the CSS
      for now.
    - use a relative span to position the remove button span within the
      input box.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,51 @@
 /*global define */
-define([], function () {
+define(['jquery'], function($) {
     'use strict';
-
-    return '\'Allo \'Allo!';
+    var clickHandler = function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            var $target = $(evt.target);
+            if ($target[0].disabled) {
+                return;
+            }
+            var f = $target.closest('form');
+            // Disable button for 1000ms after submission to debounce
+            // and avoid double clicks
+            $target.addClass('disabled_button')[0].disabled = true;
+            f.trigger('submit');
+            setTimeout(function() {
+                $target.removeClass('disabled_button')[0].disabled = false;
+            }, 1000);
+        };
+    var init = function() {
+            var $button = $('.login_button');
+            $button.on('click', clickHandler);
+            var $username = $('.username input');
+            $username.on('focus', function(evt) {
+                console.log('focus');
+                var $target = $(evt.target);
+                if ($target.val().length > 0) {
+                    console.log('removable');
+                    $target.parent().find('.remove_button').css('visibility', 'visible');
+                }
+            });
+            $username.on('keyup', function(evt) {
+                var $target = $(evt.target);
+                if ($target.val().length > 0) {
+                    console.log('removable');
+                    $target.parent().find('.remove_button').css('visibility', 'visible');
+                } else {
+                    $target.parent().find('.remove_button').css('visibility', 'hidden');
+                }
+            });
+            $username.on('blur', function() {
+                console.log('blur');
+            });
+            $('.username .remove_button').on('click', function(evt) {
+                $(evt.target).closest('.remove_button').css('visibility', 'hidden');
+                $username.val('').trigger('focus');
+            });
+        };
+    $(init());
+    return 'Ready';
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,17 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
-        bootstrap: 'vendor/bootstrap'
     },
     shim: {
-        bootstrap: {
+        app: {
             deps: ['jquery'],
             exports: 'jquery'
         }
     }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['app'], function (app) {
     'use strict';
     // use app here
     console.log(app);
-    console.log('Running jQuery %s', $().jquery);
 });
